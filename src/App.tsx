656e2692cba5a56fa7e877557d681169b689bfe6
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 <irene.anran.niu@gmail.com>
2021-09-16 12:43:54 +1200

    Customize style with craco and less

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Menu, Breadcrumb, Form, Switch } from 'antd';
+import './App.less'
+
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['host']} style={{fontSize: "1rem"}}>
+        <Menu.Item key="create">
+          Create Kahoot
+        </Menu.Item>
+        <Menu.Item key="host">
+          Host Kahoot
+        </Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 5rem' }}>
+      <Breadcrumb style={{ margin: '1.5rem 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item>
+      </Breadcrumb>
+      <div className="site-layout-content">Content 
+      <Form.Item label="开关">
+        <Switch defaultChecked />
+      </Form.Item>
+      </div>
+    </Content>
+  </Layout>
   );
 }
 
